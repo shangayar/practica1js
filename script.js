3da1c9a44e379c53cd0a423b8b61da9204963db0
Cristian Sanhueza <40905264+shangayar@users.noreply.github.com>
2021-05-24 23:36:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+alert("Bienvenido a la primer práctica de JavaScript");
+primera=parseFloat(prompt("Ingrese el primer valor"));
+//primera=40;
+//segunda=16;
+segunda=parseFloat(prompt("Ingrese el primer valor"));
+sumatotal=primera+segunda;
+document.write("<h2>El resultado de la suma es: </h2>"+sumatotal);
